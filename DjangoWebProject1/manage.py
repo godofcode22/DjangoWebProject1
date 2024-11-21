bc14d3e21bf235131999807279307280bab600de
godofcode22 <demollinedo@gmail.com>
2024-11-21 17:44:22 -0600

    inicializacion

--- DjangoWebProject1/manage.py
+++ DjangoWebProject1/manage.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         'DjangoWebProject1.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as exc: 
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
